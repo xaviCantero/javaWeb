e1a34a8df599dd581f70ed125556a33ce800eacb
Xavi <xavicanteros@gmail.com>
2022-04-07 00:32:08 -0300

    Se agrega el proyecto

---
+++ src/main/java/ar/org/casa/java/servicio/PersonaServiceImp.java
@@ -0,0 +1,55 @@
+package ar.org.casa.java.servicio;
+
+import ar.org.casa.java.datos.PersonaDao;
+import ar.org.casa.java.domain.Persona;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+@Stateless
+public class PersonaServiceImp implements PersonaServiceRemote, PersonaService{
+
+    @Inject
+    private PersonaDao personaDao;
+    
+    @Resource
+    private SessionContext contexto;
+            
+    @Override
+    public List<Persona> listarPersonas() {
+        return personaDao.findAllPersonas();
+    }
+
+    @Override
+    public Persona encontrarPersonaPorId(Persona persona) {
+        return personaDao.findPersonaById(persona);
+    }
+
+    @Override
+    public Persona encontrarPersonaPorEmail(Persona persona) {
+        return personaDao.findPersonaByEmail(persona);
+    }
+
+    @Override
+    public void registrarPersona(Persona persona) {
+        personaDao.insertPersona(persona);
+    }
+
+    @Override
+    public void modificarPersona(Persona persona) {
+        try{
+            personaDao.updatePersona(persona);
+        }catch(Throwable t){
+            contexto.setRollbackOnly();
+            t.printStackTrace(System.out);
+        }
+    }
+
+    @Override
+    public void eliminarPersona(Persona persona) {
+        personaDao.deletePersona(persona);
+    }
+    
+}
